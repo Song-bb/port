1a3b75974d5fcc79ed06a36e5b7763396da76d46
FX570 <FX570@192.168.0.129>
2020-08-04 15:12:20 +0900

    리뷰 DB 추가중

---
+++ ex1/src/main/java/com/example/demo/dto/dto_review.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class dto_review {
+	
+	private String idx;
+	private String user_index;
+	private String item_number;
+	private String review_content;
+	private String review_score;
+	private String Buy_number;
+	private String Buy_price;
+	private String Buy_date;
+
+}
