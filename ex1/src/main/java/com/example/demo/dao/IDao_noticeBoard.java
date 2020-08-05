7628565eeb60005e6b75b7ca0066d2f56e159c48
FX570 <FX570@192.168.0.129>
2020-08-05 11:59:50 +0900

    관리자 공지사항 DB 수정

--- ex1/src/main/java/com/example/demo/dao/IDao_noticeBoard.java
+++ ex1/src/main/java/com/example/demo/dao/IDao_noticeBoard.java
@@ -1,6 +1,7 @@
 package com.example.demo.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -17,5 +18,7 @@ public interface IDao_noticeBoard {
 	public List<dto_noticeBoard> search1(String search_text );
 	public List<dto_noticeBoard> search2(String search_text );
 	public List<dto_noticeBoard> search3(String search_text );
+	public int notice_insert(Map<String, String> map);
+    public int notice_update(Map<String, String> map);
 	
 }
