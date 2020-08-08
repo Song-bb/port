ef713fefc31f91d67805a1d0b0d031e2c359f228
FX570 <FX570@192.168.25.13>
2020-08-08 11:56:15 +0900

    정기배송 서비스파일 수정

--- ex1/src/main/java/com/example/demo/Service/Service_items.java
+++ ex1/src/main/java/com/example/demo/Service/Service_items.java
@@ -1,6 +1,5 @@
 package com.example.demo.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
--- ex1/src/main/java/com/example/demo/Service/Service_regular_order.java
+++ ex1/src/main/java/com/example/demo/Service/Service_regular_order.java
@@ -1,8 +1,29 @@
 package com.example.demo.Service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.IDao_regular_order;
+import com.example.demo.dto.dto_regular_order;
 
 @Service
 public class Service_regular_order {
+	
+	@Autowired
+	IDao_regular_order dao_regular_order;
+	
+	public List<dto_regular_order> detail_idx_read(String user_id){
+		return dao_regular_order.regular_order_list(user_id);
+	}
+	
+	public int regular_order_insert(Map<String, String> map) {
+		int nInsertCount = dao_regular_order.regular_order_insert(map);
+		return nInsertCount;
+	}
+	
+	
 
 }
